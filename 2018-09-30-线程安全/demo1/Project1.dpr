a18e205299324c065ac8b94e441523bc8073660c
江湖味道 <admin@coder1631.com>
2018-10-14 23:40:59 +0800

    生产消费模型

---
+++ 2018-09-30-线程安全/demo1/Project1.dpr
@@ -0,0 +1,15 @@
+program Project1;
+
+uses
+  Vcl.Forms,
+  Unit1 in 'Unit1.pas' {Form1},
+  uSyncThread in 'uSyncThread.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.

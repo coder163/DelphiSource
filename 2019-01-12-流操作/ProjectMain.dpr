d07a750580bb1bae7dd6ef97393475e924413417
舞动的代码 <admin@coder163.com>
2019-01-13 15:54:53 +0800

    更新

---
+++ 2019-01-12-流操作/ProjectMain.dpr
@@ -0,0 +1,14 @@
+program ProjectMain;
+
+uses
+  Vcl.Forms,
+  frmMain in 'frmMain.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.

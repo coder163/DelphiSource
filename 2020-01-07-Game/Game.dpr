e723680e50764d460b486d64cda6c83b2aed15d5
舞动的代码 <admin@coder163.com>
2020-01-07 20:49:48 +0800

    更新59-俄罗斯方块-集成日和窗体的键盘事件

---
+++ 2020-01-07-Game/Game.dpr
@@ -0,0 +1,14 @@
+program Game;
+
+uses
+  Vcl.Forms,
+  MainFrm in 'MainFrm.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
